304d2eb6c3170cc73c0259fc803b2b96d240f501
Fabian Stahl <fabian.m.stahl@posteo.de>
2015-03-30 23:54:23 +0200

    Update TOOL_PATH

--- firmware/final/enviroment.mk
+++ firmware/final/enviroment.mk
@@ -1,6 +1,5 @@
 #Toolchain Path
-#Set TOOL_PATH
-TOOL_PATH ?= /home/stahl/Repos/Github/SkylineBadge/toolchain/binary/avr8-gnu-toolchain-linux_x86_64/bin/
+TOOL_PATH ?= ../../toolchain/binary/avr8-gnu-toolchain-linux_x86_64/bin/
 SHELL = sh
 CC = $(TOOL_PATH)avr-gcc
 OBJCOPY = $(TOOL_PATH)avr-objcopy
