c92be7c330f7b67047cc65dac429f642ff5b349e
Fabian Stahl <fabian.m.stahl@posteo.de>
2015-03-30 23:55:14 +0200

    Delete unnecessary variables

--- firmware/final/config.h
+++ firmware/final/config.h
@@ -1,12 +1,13 @@
 #ifndef CONFIG_H_
 #define CONFIG_H_
 
-const char* row0 = "*cccffm*";
-const char* row1 = "  fast  ";
+#define LEFT 0
+#define RIGHT 1
+
+char* row0 = "*cccffm*";
+char* row1 = "  fast  ";
 
 uint8_t scroll0 = 0;
-uint8_t scroll1 = 0;
-uint8_t blink0 = 1;
-uint8_t blink1 = 0;
+uint8_t scroll1 = 1;
 
 #endif
